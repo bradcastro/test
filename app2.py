09aca1a0b187241ce0aa8b1a0a09f0a05e668197
bradcastro <134321688+bradcastro@users.noreply.github.com>
2023-07-10 11:43:37 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,18 +3,10 @@ import pandas as pd
 from binance.client import Client
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import talib.abstract as ta
 import logging
 import math
 import ccxt
-import talib.abstract as ta
-from pandas import DataFrame
-from freqtrade.strategy.interface import IStrategy
-import qtpylib.indicators as qtpylib
-
-
-class SimpleMA_strategy(IStrategy):
-    pass
-    # your strategy code here
 
 logging.basicConfig(filename='trading_bot.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
@@ -24,42 +16,65 @@ client = Client(api_key, api_secret)
 
 trading_pairs = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT']
 
-def get_historical_data(symbol, interval, start_time, end_time):
+def calculate_sma(data, window):
+    return data['close'].rolling(window=window).mean()
+
+def get_historical_data(symbol, interval):
     try:
-        # your code here
+        bars = client.get_historical_klines(symbol, interval, "1 month ago UTC")
+        data = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base', 'taker_buy_quote', 'ignored'])
+        data['close'] = pd.to_numeric(data['close'])
+        return data
     except Exception as e:
         logging.error(f"Error getting historical data: {e}")
         raise e
 
 def train_model(data):
     try:
-        # your code here
+        data['SMA'] = calculate_sma(data, 14)
+        data = data.dropna()
+        
+        Y = (data['close'].shift(-1) > data['close']).astype(int)
+        X = data.drop('date', axis=1)
+        
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        model = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=42)
+        model.fit(X_train, Y_train)
+        
+        logging.info(f"Model Score: {model.score(X_test, Y_test)}")
+        return model
     except Exception as e:
         logging.error(f"Error training model: {e}")
         raise e
 
 def execute_trade(symbol, side, cost, order_type=Client.ORDER_TYPE_MARKET):
     try:
-        # your code here
+        order = {}
+        if side == "buy":
+            order = client.order_market_buy(symbol=symbol, quantity=cost)
+        elif side == "sell":
+            order = client.order_market_sell(symbol=symbol, quantity=cost)
+        return order
     except Exception as e:
         logging.error(f"Error executing trade: {e}")
         raise e
 
 def main():
     for pair in trading_pairs:
-        data = get_historical_data(pair, Client.KLINE_INTERVAL_1HOUR, "90 days ago UTC", "now UTC")
+        data = get_historical_data(pair, Client.KLINE_INTERVAL_1DAY)
 
         model = train_model(data)
 
-        latest_data = get_historical_data(pair, Client.KLINE_INTERVAL_1HOUR, "1 hour ago UTC", "now UTC")
-
-        X_latest = pd.concat([latest_data['close'].pct_change(), latest_data['volume'].pct_change()], axis=1).dropna()
+        latest_data = get_historical_data(pair, Client.KLINE_INTERVAL_1DAY)
+        latest_data['SMA'] = calculate_sma(latest_data, 14)
+        X_latest = latest_data.drop('date', axis=1).dropna()
+        
         prediction = model.predict(X_latest.values.reshape(1, -1))
 
         if prediction[0] == 1:
-            execute_trade(pair, Client.SIDE_BUY, 100)
+            execute_trade(pair, "buy", 0.001)
         elif prediction[0] == 0:
-            execute_trade(pair, Client.SIDE_SELL, 100)
+            execute_trade(pair, "sell", 0.001)
 
 if __name__ == '__main__':
     main()
